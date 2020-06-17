436fc10beb336d528290723d6f59064728becd29
HS_Dev <HS_Dev@192.168.1.6>
2020-06-18 00:50:16 +0530

    lambok jar added

--- ShoppingCartProject/src/main/java/com/shopingCart/productVo/ProductPriceDetails.java
+++ ShoppingCartProject/src/main/java/com/shopingCart/productVo/ProductPriceDetails.java
@@ -6,31 +6,21 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+@Data
+@Getter
+@Setter
 @Entity
 @Table(name="productPriceDetails")
 public class ProductPriceDetails {
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private long productId;
-	public long getProductId() {
-		return productId;
-	}
-	public void setProductId(long productId) {
-		this.productId = productId;
-	}
+	
 	private String productName;
 	private double price;
-	public String getProductName() {
-		return productName;
-	}
-	public void setProductName(String productName) {
-		this.productName = productName;
-	}
-	public double getPrice() {
-		return price;
-	}
-	public void setPrice(double price) {
-		this.price = price;
-	}
+	
 	
 }

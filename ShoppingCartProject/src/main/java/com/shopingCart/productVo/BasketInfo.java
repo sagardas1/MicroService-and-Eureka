d36bd6c83e55652e0dd2a82ccfff6fbaeb067920
sagardas1 <sagardas.195@gmail.com>
2020-07-04 21:59:19 +0530

    changes

--- ShoppingCartProject/src/main/java/com/shopingCart/productVo/BasketInfo.java
+++ ShoppingCartProject/src/main/java/com/shopingCart/productVo/BasketInfo.java
@@ -2,13 +2,10 @@ package com.shopingCart.productVo;
 
 import java.util.List;
 
-import com.shopingCart.baseResponce.BaseResponce;
 import com.shopingCart.userVo.RegistrationVo;
 
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
-@Data
 @Getter
 @Setter
 public class BasketInfo extends RegistrationVo{

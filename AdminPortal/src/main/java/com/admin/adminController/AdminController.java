cb6bf1f0de860080850b371eaa54adedf94157fc
sagardas1 <sagardas.195@gmail.com>
2020-08-02 01:21:37 +0530

    added new controller.

--- AdminPortal/src/main/java/com/admin/adminController/AdminController.java
+++ AdminPortal/src/main/java/com/admin/adminController/AdminController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -64,5 +65,13 @@ public class AdminController {
 		List<ProductVo> product=adminService.allProducthavingQuantitygreaterThan5();
 		productList.setProductList(product);
 		return productList;}
+	
+	
+	@GetMapping(value="/{name}")
+	public String getLastName(@PathVariable String name){
+		System.out.println(name);
+		return name;
+	}
+
 
 }

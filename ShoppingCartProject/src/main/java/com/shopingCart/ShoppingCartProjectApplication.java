7eebfd904e237b3ce893cf212d2044852278565b
sagardas1 <sagardas.195@gmail.com>
2020-08-06 23:45:20 +0530

    CHANGES

--- ShoppingCartProject/src/main/java/com/shopingCart/ShoppingCartProjectApplication.java
+++ ShoppingCartProject/src/main/java/com/shopingCart/ShoppingCartProjectApplication.java
@@ -1,9 +1,16 @@
 package com.shopingCart;
 
+import java.util.Collections;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
@@ -17,4 +24,25 @@ public class ShoppingCartProjectApplication {
 		SpringApplication.run(ShoppingCartProjectApplication.class, args);
 	}
 
+	public Docket swaggerConfiguration() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.paths(PathSelectors.ant("/api/*"))
+				.apis(RequestHandlerSelectors.basePackage("sagar das"))
+				
+				.build()
+				.apiInfo(apiDetails());
+		
+	}
+	
+	private ApiInfo apiDetails() {
+		return new ApiInfo(
+				"Shopping App",
+				"sample Api",
+				" 1.0", 
+				"free to use",
+				new springfox.documentation.service.Contact("SAGAR DAS", "WWW.VOID.COM", "SAGARDAS.195@GMAIL.COM"),
+				"hdsgf", 
+				"GVG",
+				Collections.emptyList());
+	}
 }

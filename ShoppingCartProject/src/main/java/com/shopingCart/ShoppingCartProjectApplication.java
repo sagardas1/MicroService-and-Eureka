0ba3c5732363e946d6ae9b29f9b0223fdae2f5f9
sagardas1 <sagardas.195@gmail.com>
2020-08-07 20:38:52 +0530

    changes

--- ShoppingCartProject/src/main/java/com/shopingCart/ShoppingCartProjectApplication.java
+++ ShoppingCartProject/src/main/java/com/shopingCart/ShoppingCartProjectApplication.java
@@ -1,14 +1,10 @@
 package com.shopingCart;
 
-import java.util.Collections;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -32,6 +28,7 @@ public class ShoppingCartProjectApplication {
 		
 	}
 	
+	@SuppressWarnings("deprecation")
 	@Bean
 	private static ApiInfo apiDetails() {
 		return new ApiInfo("Shopping Application", "Apis for Shopping-Cart. Copyright by Sagar Das", "Team", "sdbfhsd", "jsdbfj", "@void", "hsdbf");

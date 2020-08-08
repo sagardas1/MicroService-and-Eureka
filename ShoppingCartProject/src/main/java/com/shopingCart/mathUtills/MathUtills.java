73773495397d54bdedab10fdfef4d6d8e773ceb0
sagardas1 <sagardas.195@gmail.com>
2020-08-08 22:10:55 +0530

    changes

---
+++ ShoppingCartProject/src/main/java/com/shopingCart/mathUtills/MathUtills.java
@@ -0,0 +1,27 @@
+package com.shopingCart.mathUtills;
+
+public class MathUtills {
+
+	
+	public int add(int a,int b) {
+		return a+b;
+	}
+	
+	public int sub(int a,int b) {
+		if(a>b) {return a-b;}else return b-a;
+	}
+	
+	public int multi(int a,int b) {
+		return a*b;
+	}
+	
+	
+	public int div(int a,int b) {
+		return a/b;
+	}
+	
+	public int modulo(int a,int b) {
+		return a%b;
+	}
+}
+

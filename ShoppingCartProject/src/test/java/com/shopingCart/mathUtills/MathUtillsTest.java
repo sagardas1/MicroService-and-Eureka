c840828a89691579056d0354c7387f0d303339bb
sagardas1 <sagardas.195@gmail.com>
2020-08-08 23:26:39 +0530

    implemented junit5 in project.

---
+++ ShoppingCartProject/src/test/java/com/shopingCart/mathUtills/MathUtillsTest.java
@@ -0,0 +1,13 @@
+package com.shopingCart.mathUtills;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MathUtillsTest {
+
+	@Test
+	void test() {
+		System.out.println("this test run");	}
+
+}

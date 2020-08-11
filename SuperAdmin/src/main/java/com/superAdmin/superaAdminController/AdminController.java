57cb05ff078126fd1dc3eb3c595135d734ffe891
sagardas1 <sagardas.195@gmail.com>
2020-08-12 01:03:11 +0530

    changes.

--- SuperAdmin/src/main/java/com/superAdmin/superaAdminController/AdminController.java
+++ SuperAdmin/src/main/java/com/superAdmin/superaAdminController/AdminController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.superAdmin.adminService.AdminService;
 import com.superAdmin.productVo.ProductPriceDetails;
 

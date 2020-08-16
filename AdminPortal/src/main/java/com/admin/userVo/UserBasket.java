830cbc5934556fd008c99572499ac5f25e58fa7b
sagardas1 <sagardas.195@gmail.com>
2020-08-16 14:26:29 +0530

    changes

--- AdminPortal/src/main/java/com/admin/userVo/UserBasket.java
+++ AdminPortal/src/main/java/com/admin/userVo/UserBasket.java
@@ -1,13 +1,8 @@
 package com.admin.userVo;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
 public class UserBasket {
 	
+	@SuppressWarnings("unused")
 	private long id;
 	private String email;
 	private String productName;

8a631162895606e27615103a58abdcd41907f083
sagardas1 <sagardas.195@gmail.com>
2020-08-16 14:19:37 +0530

    deleting imports

--- AdminPortal/src/main/java/com/admin/adminController/AdminController.java
+++ AdminPortal/src/main/java/com/admin/adminController/AdminController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.admin.adminService.AdminService;
 import com.admin.baseResponce.BaseResponce;

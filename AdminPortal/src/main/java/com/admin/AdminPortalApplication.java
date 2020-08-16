91dcb78e3145fed3c0c2f7e6ef8d49e3855dc288
sagardas1 <sagardas.195@gmail.com>
2020-08-16 14:19:12 +0530

    changes

--- AdminPortal/src/main/java/com/admin/AdminPortalApplication.java
+++ AdminPortal/src/main/java/com/admin/AdminPortalApplication.java
@@ -2,11 +2,9 @@ package com.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.reactive.function.client.WebClientAutoConfiguration;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 

863bbe75e0dbc837862e2e55b6cc25c61026f1f2
sagardas1 <sagardas.195@gmail.com>
2020-08-16 14:22:20 +0530

    use of lombok.

--- AdminPortal/src/main/java/com/admin/productVo/BasketInfo.java
+++ AdminPortal/src/main/java/com/admin/productVo/BasketInfo.java
@@ -4,11 +4,9 @@ import java.util.List;
 
 import com.admin.userVo.RegistrationVo;
 
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
-@Data
 @Getter
 @Setter
 public class BasketInfo extends RegistrationVo{

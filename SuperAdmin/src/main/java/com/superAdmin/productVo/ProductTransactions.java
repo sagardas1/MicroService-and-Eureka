7b29fea63ce2ebc909b567f387f2f5f149ea9de6
sagardas1 <sagardas.195@gmail.com>
2020-08-17 21:18:11 +0530

    changes

--- SuperAdmin/src/main/java/com/superAdmin/productVo/ProductTransactions.java
+++ SuperAdmin/src/main/java/com/superAdmin/productVo/ProductTransactions.java
@@ -1,12 +1,8 @@
 package com.superAdmin.productVo;
 
-import java.util.List;
-
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
-@Data
 @Getter
 @Setter
 public class ProductTransactions  {

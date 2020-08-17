45d7a902c4d11b5e8a59f1fab951f6ad48fcf978
sagardas1 <sagardas.195@gmail.com>
2020-08-17 21:17:42 +0530

    changes

--- SuperAdmin/src/main/java/com/superAdmin/productVo/BasketInfo.java
+++ SuperAdmin/src/main/java/com/superAdmin/productVo/BasketInfo.java
@@ -4,11 +4,9 @@ import java.util.List;
 
 import com.superAdmin.userVo.RegistrationVo;
 
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
-@Data
 @Getter
 @Setter
 public class BasketInfo extends RegistrationVo{

8a7efbbce3e0e2ce1533829d6b666ca3060241f7
sagardas1 <sagardas.195@gmail.com>
2020-08-18 15:06:19 +0530

    changes

--- ShoppingCartProject/src/main/java/com/shopingCart/productDao/ProductDao.java
+++ ShoppingCartProject/src/main/java/com/shopingCart/productDao/ProductDao.java
@@ -14,26 +14,25 @@ import com.shopingCart.productVo.ProductVo;
 
 @Repository
 @Transactional
-public interface ProductDao extends CrudRepository<ProductVo,Long>{
+public interface ProductDao extends CrudRepository<ProductVo, Long> {
 
 	@Transactional
-	@Query(value="select * from productBaseDate where productName=:productName",nativeQuery=true)
-	ProductVo getproductFromdb(@Param("productName")String productName);
-	
-	
+	@Query(value = "select * from productBaseDate where productName=:productName", nativeQuery = true)
+	ProductVo getproductFromdb(@Param("productName") String productName);
+
 	@Modifying
 	@Transactional
-	@Query(value="update productBaseDate set quantity=:quantity "
-			+ "where productName=:productName",nativeQuery=true)
-	int updateProductsQuantity(@Param("productName")String productName,@Param("quantity")long quantity);
+	@Query(value = "update productBaseDate set quantity=:quantity "
+			+ "where productName=:productName", nativeQuery = true)
+	int updateProductsQuantity(@Param("productName") String productName, @Param("quantity") long quantity);
 
 	@Transactional
-	@Query(value="select * from productBaseDate where quantity>0",nativeQuery=true)
+	@Query(value = "select * from productBaseDate where quantity>0", nativeQuery = true)
 	List<ProductVo> viewAllProduct();
-@Modifying
-	@Transactional
-	@Query(value="update productBaseDate set quantity=:quantity where productName=:productName",nativeQuery=true)
-	void updateRecord(@Param("productName")String productName,@Param("quantity") long quantity);
 
+	@Modifying
+	@Transactional
+	@Query(value = "update productBaseDate set quantity=:quantity where productName=:productName", nativeQuery = true)
+	void updateRecord(@Param("productName") String productName, @Param("quantity") long quantity);
 
 }

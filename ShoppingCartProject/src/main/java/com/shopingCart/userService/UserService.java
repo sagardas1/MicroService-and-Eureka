253a964429c6c748da4cd7d1d4a5ede2c6854eb8
sagardas1 <sagardas.195@gmail.com>
2020-08-21 18:35:59 +0530

    changes

--- ShoppingCartProject/src/main/java/com/shopingCart/userService/UserService.java
+++ ShoppingCartProject/src/main/java/com/shopingCart/userService/UserService.java
@@ -155,7 +155,7 @@ public class UserService {
 	}
 
 	public BaseResponce<Void> checkOtp(String otp) {
-		BaseResponce baseResponce = new BaseResponce();
+		BaseResponce<Void> baseResponce = new BaseResponce();
 		if (otp.equals(OTP)) {
 			baseResponce.setStatusCode(ResponceConstants.SUCCESS_CREATED);
 			baseResponce.setStatusMessage(ResponceConstants.SUCESS_MESSAGE);

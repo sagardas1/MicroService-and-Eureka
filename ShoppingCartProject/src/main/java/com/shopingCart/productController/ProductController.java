668588492e548111f822892fbd645d18b881f4c0
sagardas1 <sagardas.195@gmail.com>
2020-08-23 19:56:31 +0530

    new service added.

--- ShoppingCartProject/src/main/java/com/shopingCart/productController/ProductController.java
+++ ShoppingCartProject/src/main/java/com/shopingCart/productController/ProductController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -132,6 +133,12 @@ public class ProductController {
 			e.printStackTrace();
 		}
 		return product;
+	}
+	
+	@GetMapping(value="/productName/{name}", headers = "Accept=application/json")
+	public String getProductName(@PathVariable(value="name")String name) {
+		return name;
+		
 	}
 
 }

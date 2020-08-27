70104e4f8ad7cba7e947abaec11d474892dff28c
sagardas1 <sagardas.195@gmail.com>
2020-08-27 12:25:02 +0530

    changes

--- ShoppingCartProject/src/main/java/com/shopingCart/userVo/UserBasket.java
+++ ShoppingCartProject/src/main/java/com/shopingCart/userVo/UserBasket.java
@@ -7,34 +7,37 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="userbasket")
+@Table(name = "userbasket")
 public class UserBasket {
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	private String email;
 	private String productName;
 	private long quantity;
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getProductName() {
 		return productName;
 	}
+
 	public void setProductName(String productName) {
 		this.productName = productName;
 	}
+
 	public long getQuantity() {
 		return quantity;
 	}
+
 	public void setQuantity(long quantity) {
 		this.quantity = quantity;
 	}
-	
-	
-	
 
 }

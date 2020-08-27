1a106f925edd2615dd1a6652d05c83cd29eb4362
sagardas1 <sagardas.195@gmail.com>
2020-08-27 12:24:22 +0530

    changes

--- ShoppingCartProject/src/main/java/com/shopingCart/userVo/RegistrationVo.java
+++ ShoppingCartProject/src/main/java/com/shopingCart/userVo/RegistrationVo.java
@@ -9,10 +9,10 @@ import javax.persistence.Table;
 import com.shopingCart.baseResponce.BaseResponce;
 
 @Entity
-@Table(name="registration")
-public class RegistrationVo extends BaseResponce<Object>{
+@Table(name = "registration")
+public class RegistrationVo extends BaseResponce<Object> {
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long userId;
 	private String name;
 	private String address;
@@ -21,55 +21,69 @@ public class RegistrationVo extends BaseResponce<Object>{
 	private String confirmPassword;
 	private String email;
 	private int roleId;
+
 	public int getRoleId() {
 		return roleId;
 	}
+
 	public void setRoleId(int roleId) {
 		this.roleId = roleId;
 	}
+
 	public long getUserId() {
 		return userId;
 	}
+
 	public void setUserId(long userId) {
 		this.userId = userId;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getAddress() {
 		return address;
 	}
+
 	public void setAddress(String address) {
 		this.address = address;
 	}
+
 	public String getContactNumber() {
 		return contactNumber;
 	}
+
 	public void setContactNumber(String contactNumber) {
 		this.contactNumber = contactNumber;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	
+
 	public String getConfirmPassword() {
 		return confirmPassword;
 	}
+
 	public void setConfirmPassword(String confirmPassword) {
 		this.confirmPassword = confirmPassword;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	
 
 }

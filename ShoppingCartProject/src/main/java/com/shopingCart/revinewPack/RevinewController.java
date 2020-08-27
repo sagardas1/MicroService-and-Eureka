1a486a415c27e30a90ae4b0de163e29f5cd99390
sagardas1 <sagardas.195@gmail.com>
2020-08-27 12:21:03 +0530

    changes

--- ShoppingCartProject/src/main/java/com/shopingCart/revinewPack/RevinewController.java
+++ ShoppingCartProject/src/main/java/com/shopingCart/revinewPack/RevinewController.java
@@ -4,11 +4,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController 
+@RestController
 @RequestMapping("/revenue")
 public class RevinewController {
-	
-	@GetMapping(value="/getrevenue")
+
+	@GetMapping(value = "/getrevenue")
 	public int getAllRevinew() {
 		return 10;
 	}

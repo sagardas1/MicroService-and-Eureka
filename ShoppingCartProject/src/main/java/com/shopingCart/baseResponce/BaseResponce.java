d7dc3b7758283d6184a327d17d77ffcb2dee5da6
sagardas1 <sagardas.195@gmail.com>
2020-08-27 12:18:49 +0530

    changes

--- ShoppingCartProject/src/main/java/com/shopingCart/baseResponce/BaseResponce.java
+++ ShoppingCartProject/src/main/java/com/shopingCart/baseResponce/BaseResponce.java
@@ -2,33 +2,41 @@ package com.shopingCart.baseResponce;
 
 public class BaseResponce<T> {
 	private T t;
-	
+
 	private String statusMessage;
 	private long statusCode;
 	private long timeStamp;
+
 	public String getStatusMessage() {
 		return statusMessage;
 	}
+
 	public void setStatusMessage(String statusMessage) {
 		this.statusMessage = statusMessage;
 	}
+
 	public long getStatusCode() {
 		return statusCode;
 	}
+
 	public void setStatusCode(long statusCode) {
 		this.statusCode = statusCode;
 	}
+
 	public T getT() {
 		return t;
 	}
+
 	public void setT(T t) {
 		this.t = t;
 	}
+
 	public long getTimeStamp() {
 		return timeStamp;
 	}
+
 	public void setTimeStamp(long timeStamp) {
 		this.timeStamp = timeStamp;
 	}
-	
+
 }

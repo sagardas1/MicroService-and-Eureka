3e64a7c2da7eb903d165c5994fc5c40cee5a8748
sagardas1 <sagardas.195@gmail.com>
2020-08-27 12:22:57 +0530

    changes

--- ShoppingCartProject/src/main/java/com/shopingCart/userVo/LoginVo.java
+++ ShoppingCartProject/src/main/java/com/shopingCart/userVo/LoginVo.java
@@ -7,22 +7,26 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="login")
+@Table(name = "login")
 public class LoginVo {
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	private String email;
 	private String password;
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}

c136c694e7f825f1d39e4e07e904ec46a2627109
sagardas1 <sagardas.195@gmail.com>
2020-09-20 17:25:19 +0530

    changes

--- ShoppingCartProject/src/main/java/com/shopingCart/productDao/ProductPriceDetailsDao.java
+++ ShoppingCartProject/src/main/java/com/shopingCart/productDao/ProductPriceDetailsDao.java
@@ -8,21 +8,17 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.shopingCart.productVo.ProductPriceDetails;
+
 @Repository
 @Transactional
-public interface ProductPriceDetailsDao  extends CrudRepository<ProductPriceDetails,Long>{
+public interface ProductPriceDetailsDao extends CrudRepository<ProductPriceDetails, Long> {
 
 	@Transactional
-	@Query(value="select * from productPriceDetails where productName=:name",nativeQuery=true)
-	ProductPriceDetails getProductPrice(@Param("name")String name);
+	@Query(value = "select * from productPriceDetails where productName=:name", nativeQuery = true)
+	ProductPriceDetails getProductPrice(@Param("name") String name);
 
-	
 	@Transactional
-	@Query(value="DELETE FROM productPriceDetails WHERE productName:name",nativeQuery=true)
+	@Query(value = "DELETE FROM productPriceDetails WHERE productName:name", nativeQuery = true)
 	ProductPriceDetails deleteProduct(String name);
-
-
-	
-	
 
 }

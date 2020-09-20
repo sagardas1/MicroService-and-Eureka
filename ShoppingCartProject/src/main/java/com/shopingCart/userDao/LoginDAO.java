3fda0b0a22def104e82bc0424883e0ea8ffee7d6
sagardas1 <sagardas.195@gmail.com>
2020-09-20 17:27:36 +0530

    changes

--- ShoppingCartProject/src/main/java/com/shopingCart/userDao/LoginDAO.java
+++ ShoppingCartProject/src/main/java/com/shopingCart/userDao/LoginDAO.java
@@ -10,25 +10,21 @@ import org.springframework.stereotype.Repository;
 
 import com.shopingCart.userVo.LoginVo;
 
-
 @Repository
 @Transactional
-public interface LoginDAO  extends CrudRepository<LoginVo,Long>{
-	
+public interface LoginDAO extends CrudRepository<LoginVo, Long> {
+
 	@Transactional
-	@Query(value="select * from login where email = :email AND password = :password",nativeQuery=true)
+	@Query(value = "select * from login where email = :email AND password = :password", nativeQuery = true)
 	LoginVo findUserLogin(@Param("email") String email, @Param("password") String password);
-	
-	
-	
+
 	@Transactional
-	@Query(value="select * from login where email = :email",nativeQuery=true)
+	@Query(value = "select * from login where email = :email", nativeQuery = true)
 	LoginVo findUser(@Param("email") String email);
 
 	@Modifying
 	@Transactional
-	@Query(value="delete from login where email=:email",nativeQuery=true)
+	@Query(value = "delete from login where email=:email", nativeQuery = true)
 	void deleteLongin(String email);
-
 
 }

6d9ae2b8a63f70f7ee885a6c940d1abd272e0943
sagardas1 <sagardas.195@gmail.com>
2020-09-24 22:55:31 +0530

    chnages

--- ShoppingCartProject/src/main/java/com/shopingCart/demoServiceLayer/MyServiceImpli.java
+++ ShoppingCartProject/src/main/java/com/shopingCart/demoServiceLayer/MyServiceImpli.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service("myServiceImpli")
-public class MyServiceImpli implements MyService{
+public class MyServiceImpli implements MyService {
 	@SuppressWarnings("unused")
 	@Autowired
 	private Reposi reposi;

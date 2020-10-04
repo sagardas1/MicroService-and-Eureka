40290d4c72e8c75d3fb658f7ff89559711a696c6
sagardas1 <sagardas.195@gmail.com>
2020-10-04 22:02:46 +0530

    changes

--- SuperAdmin/src/main/java/com/superAdmin/userVo/LoginVo.java
+++ SuperAdmin/src/main/java/com/superAdmin/userVo/LoginVo.java
@@ -5,21 +5,27 @@ public class LoginVo {
 	private long id;
 	private String email;
 	private String password;
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	public long getId() {
 		return id;
 	}
+
 	public void setId(long id) {
 		this.id = id;
 	}

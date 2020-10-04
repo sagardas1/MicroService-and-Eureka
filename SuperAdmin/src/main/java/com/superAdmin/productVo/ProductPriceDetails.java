6f12044d02ebeeef045236b50dfdc3741203c1e6
sagardas1 <sagardas.195@gmail.com>
2020-10-04 22:00:25 +0530

    changes

--- SuperAdmin/src/main/java/com/superAdmin/productVo/ProductPriceDetails.java
+++ SuperAdmin/src/main/java/com/superAdmin/productVo/ProductPriceDetails.java
@@ -3,15 +3,13 @@ package com.superAdmin.productVo;
 import lombok.Getter;
 import lombok.Setter;
 
-
 @Getter
 @Setter
 public class ProductPriceDetails {
-	
+
 	private long productId;
-	
+
 	private String productName;
 	private double price;
 
-	
 }

77244ab51ff9ee21529f93c8a365493dcba50125
sagardas1 <sagardas.195@gmail.com>
2020-10-06 22:20:05 +0530

    changes

--- SuperAdmin/src/main/java/com/superAdmin/baseResponce/BaseResponce.java
+++ SuperAdmin/src/main/java/com/superAdmin/baseResponce/BaseResponce.java
@@ -4,25 +4,29 @@ public class BaseResponce<T> {
 	private T t;
 	private String statusMessage;
 	private long statusCode;
+
 	public String getStatusMessage() {
 		return statusMessage;
 	}
-	
+
 	public void setStatusMessage(String statusMessage) {
 		this.statusMessage = statusMessage;
 	}
+
 	public long getStatusCode() {
 		return statusCode;
 	}
+
 	public void setStatusCode(long statusCode) {
 		this.statusCode = statusCode;
 	}
+
 	public T getT() {
 		return t;
 	}
+
 	public void setT(T t) {
 		this.t = t;
 	}
-	
 
 }

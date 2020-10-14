2b538279747f6fc8e950e0e7a79f8fa065641997
sagardas1 <sagardas.195@gmail.com>
2020-10-14 19:48:29 +0530

    changes

--- ShoppingCartProject/src/main/java/com/shopingCart/productVo/Payment.java
+++ ShoppingCartProject/src/main/java/com/shopingCart/productVo/Payment.java
@@ -1,16 +1,34 @@
 package com.shopingCart.productVo;
 
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
-
-@Data
-@Getter
-@Setter
 public class Payment {
 	private double finalAmount;
 	private long paymentStatus;
 	private double amountPaid;
 	private double amountToBePaid;
+	public double getFinalAmount() {
+		return finalAmount;
+	}
+	public void setFinalAmount(double finalAmount) {
+		this.finalAmount = finalAmount;
+	}
+	public long getPaymentStatus() {
+		return paymentStatus;
+	}
+	public void setPaymentStatus(long paymentStatus) {
+		this.paymentStatus = paymentStatus;
+	}
+	public double getAmountPaid() {
+		return amountPaid;
+	}
+	public void setAmountPaid(double amountPaid) {
+		this.amountPaid = amountPaid;
+	}
+	public double getAmountToBePaid() {
+		return amountToBePaid;
+	}
+	public void setAmountToBePaid(double amountToBePaid) {
+		this.amountToBePaid = amountToBePaid;
+	}
+	
 
 }

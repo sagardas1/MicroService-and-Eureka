96156485c3fe6368f4a0b536d4a7d3fc03221119
sagardas1 <sagardas.195@gmail.com>
2020-10-15 18:36:02 +0530

    changes

--- ShoppingCartProject/src/main/java/com/shopingCart/demoServiceLayer/OtherServiceImpli.java
+++ ShoppingCartProject/src/main/java/com/shopingCart/demoServiceLayer/OtherServiceImpli.java
@@ -3,11 +3,10 @@ package com.shopingCart.demoServiceLayer;
 import org.springframework.stereotype.Service;
 
 @Service("otherServiceImpli")
-public class OtherServiceImpli implements MyService{
+public class OtherServiceImpli implements MyService {
 
 	@Override
 	public String getName(String name) {
-		
 		return "otherServiceImpli";
 	}
 

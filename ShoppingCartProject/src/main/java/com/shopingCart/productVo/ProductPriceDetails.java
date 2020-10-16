0884272edb81fdae220c1a80e1a46aafbee53e5c
sagardas1 <sagardas.195@gmail.com>
2020-10-16 14:58:43 +0530

    changes

--- ShoppingCartProject/src/main/java/com/shopingCart/productVo/ProductPriceDetails.java
+++ ShoppingCartProject/src/main/java/com/shopingCart/productVo/ProductPriceDetails.java
@@ -6,11 +6,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
-
-
 @Entity
 @Table(name = "productPriceDetails")
 public class ProductPriceDetails {
@@ -20,21 +15,27 @@ public class ProductPriceDetails {
 
 	private String productName;
 	private double price;
+
 	public long getProductId() {
 		return productId;
 	}
+
 	public void setProductId(long productId) {
 		this.productId = productId;
 	}
+
 	public String getProductName() {
 		return productName;
 	}
+
 	public void setProductName(String productName) {
 		this.productName = productName;
 	}
+
 	public double getPrice() {
 		return price;
 	}
+
 	public void setPrice(double price) {
 		this.price = price;
 	}

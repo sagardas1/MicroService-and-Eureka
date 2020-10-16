cd580197a3131091ce2cb43f0659e76fc280454a
sagardas1 <sagardas.195@gmail.com>
2020-10-16 14:57:24 +0530

    changes

--- ShoppingCartProject/src/main/java/com/shopingCart/productVo/BasketInfo.java
+++ ShoppingCartProject/src/main/java/com/shopingCart/productVo/BasketInfo.java
@@ -4,31 +4,34 @@ import java.util.List;
 
 import com.shopingCart.userVo.RegistrationVo;
 
-
 public class BasketInfo extends RegistrationVo {
-	
-	
+
 	private List<ItemVo> itemList;
 	private double totalPrice;
 	private Payment payment;
+
 	public List<ItemVo> getItemList() {
 		return itemList;
 	}
+
 	public void setItemList(List<ItemVo> itemList) {
 		this.itemList = itemList;
 	}
+
 	public double getTotalPrice() {
 		return totalPrice;
 	}
+
 	public void setTotalPrice(double totalPrice) {
 		this.totalPrice = totalPrice;
 	}
+
 	public Payment getPayment() {
 		return payment;
 	}
+
 	public void setPayment(Payment payment) {
 		this.payment = payment;
 	}
 
-	
 }

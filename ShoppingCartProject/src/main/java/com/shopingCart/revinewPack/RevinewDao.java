1fcb9fecb1c9b88eefd894deccfac89756144173
sagardas1 <sagardas.195@gmail.com>
2020-10-22 15:15:08 +0530

    changes

--- ShoppingCartProject/src/main/java/com/shopingCart/revinewPack/RevinewDao.java
+++ ShoppingCartProject/src/main/java/com/shopingCart/revinewPack/RevinewDao.java
@@ -6,11 +6,9 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.shopingCart.revinewVo.RevinewVo;
+
 @Repository
 @Transactional
-public interface RevinewDao extends CrudRepository<RevinewVo,Long>{
-	
-	
-	
+public interface RevinewDao extends CrudRepository<RevinewVo, Long> {
 
 }

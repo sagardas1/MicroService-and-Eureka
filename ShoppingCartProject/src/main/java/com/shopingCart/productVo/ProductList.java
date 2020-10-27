41a8a662ddbd0e9512cb59641469b61a5dadb27d
sagardas1 <sagardas.195@gmail.com>
2020-10-27 17:09:31 +0530

    changes

--- ShoppingCartProject/src/main/java/com/shopingCart/productVo/ProductList.java
+++ ShoppingCartProject/src/main/java/com/shopingCart/productVo/ProductList.java
@@ -5,14 +5,12 @@ import java.util.List;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
+
 @Data
 @Getter
 @Setter
 public class ProductList {
-	
-	public List<ProductVo> productList;
 
-	
-	
+	public List<ProductVo> productList;
 
 }

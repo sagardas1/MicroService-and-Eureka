77482cb93f1331a8747d03a808fc2f2da33a74bf
sagardas1 <sagardas.195@gmail.com>
2020-10-28 13:43:42 +0530

    space cleanup.

--- SuperAdmin/src/main/java/com/superAdmin/productVo/BasketInfo.java
+++ SuperAdmin/src/main/java/com/superAdmin/productVo/BasketInfo.java
@@ -10,7 +10,6 @@ import lombok.Setter;
 @Getter
 @Setter
 public class BasketInfo extends RegistrationVo {
-	
 	private List<ItemVo> itemList;
 	private double totalPrice;
 	private Payment payment;

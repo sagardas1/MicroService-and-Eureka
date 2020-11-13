17e1f5fa45e1c4027202b4608f30be9c37e45eab
sagardas1 <sagardas.195@gmail.com>
2020-11-13 23:40:28 +0530

    CHANGES

--- AdminPortal/src/main/java/com/admin/productVo/ProductList.java
+++ AdminPortal/src/main/java/com/admin/productVo/ProductList.java
@@ -3,21 +3,12 @@ package com.admin.productVo;
 import java.util.List;
 
 public class ProductList {
-	
+
 	public List<ProductVo> productList;
 
-	
-	
-	
-	
-	
-	
 	public void setProductList(List<ProductVo> product) {
 		// TODO Auto-generated method stub
-		
-		
+
 	}
-
-
 
 }

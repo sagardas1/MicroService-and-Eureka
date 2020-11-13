918bb42813a6d58a84b829a39e9f87455fdc2ec6
sagardas1 <sagardas.195@gmail.com>
2020-11-13 22:49:59 +0530

    changes

--- AdminPortal/src/main/java/com/admin/productVo/ProductList.java
+++ AdminPortal/src/main/java/com/admin/productVo/ProductList.java
@@ -2,15 +2,15 @@ package com.admin.productVo;
 
 import java.util.List;
 
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
-@Data
-@Getter
-@Setter
 public class ProductList {
 	
 	public List<ProductVo> productList;
+
+	public void setProductList(List<ProductVo> product) {
+		// TODO Auto-generated method stub
+		
+		
+	}
 
 
 

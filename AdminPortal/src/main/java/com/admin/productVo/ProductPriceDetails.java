b8b29e4bbc653aded27a7ec813f911485ce11434
sagardas1 <sagardas.195@gmail.com>
2020-11-14 22:26:55 +0530

    CHANGES

--- AdminPortal/src/main/java/com/admin/productVo/ProductPriceDetails.java
+++ AdminPortal/src/main/java/com/admin/productVo/ProductPriceDetails.java
@@ -1,16 +1,28 @@
 package com.admin.productVo;
 
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
-@Data
-@Getter
-@Setter
 public class ProductPriceDetails {
 
 	private long productId;
 	private String productName;
 	private double price;
+	public long getProductId() {
+		return productId;
+	}
+	public void setProductId(long productId) {
+		this.productId = productId;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
 
 }

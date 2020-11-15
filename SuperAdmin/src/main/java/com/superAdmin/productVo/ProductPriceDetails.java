8cebd346383472a94ab772d496f90650946c508c
sagardas1 <sagardas.195@gmail.com>
2020-11-15 21:26:59 +0530

    CHANGES

--- SuperAdmin/src/main/java/com/superAdmin/productVo/ProductPriceDetails.java
+++ SuperAdmin/src/main/java/com/superAdmin/productVo/ProductPriceDetails.java
@@ -1,14 +1,30 @@
 package com.superAdmin.productVo;
 
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
 public class ProductPriceDetails {
 
 	private long productId;
 	private String productName;
 	private double price;
+	public long getProductId() {
+		return productId;
+	}
+	public void setProductId(long productId) {
+		this.productId = productId;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	
 
 }

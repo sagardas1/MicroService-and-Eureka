859eff8acf79c71bdc045aae033264eb6b424dd8
sagardas1 <sagardas.195@gmail.com>
2020-11-15 21:29:19 +0530

    changes

--- SuperAdmin/src/main/java/com/superAdmin/productVo/ProductTransactions.java
+++ SuperAdmin/src/main/java/com/superAdmin/productVo/ProductTransactions.java
@@ -1,10 +1,6 @@
 package com.superAdmin.productVo;
 
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
 public class ProductTransactions {
 
 	private long id;
@@ -14,6 +10,36 @@ public class ProductTransactions {
 	private double finalAmount;
 	private double paymentStatus;
 	private String email;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public double getAmountPaid() {
+		return amountPaid;
+	}
+	public void setAmountPaid(double amountPaid) {
+		this.amountPaid = amountPaid;
+	}
+	public double getFinalAmount() {
+		return finalAmount;
+	}
+	public void setFinalAmount(double finalAmount) {
+		this.finalAmount = finalAmount;
+	}
+	public double getPaymentStatus() {
+		return paymentStatus;
+	}
+	public void setPaymentStatus(double paymentStatus) {
+		this.paymentStatus = paymentStatus;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
 	
 }

e7b5d694ee4adb73352b45b379097f063df83218
sagardas1 <sagardas.195@gmail.com>
2020-11-17 19:12:36 +0530

    changes

--- AdminPortal/src/main/java/com/admin/productVo/BasketInfo.java
+++ AdminPortal/src/main/java/com/admin/productVo/BasketInfo.java
@@ -4,28 +4,33 @@ import java.util.List;
 
 import com.admin.userVo.RegistrationVo;
 
+public class BasketInfo extends RegistrationVo {
+	private List<ItemVo> itemList;
+	private double totalPrice;
+	private Payment payment;
 
-public class BasketInfo extends RegistrationVo{
-private	List<ItemVo> itemList;
-private double totalPrice;
-private Payment payment;
-public List<ItemVo> getItemList() {
-	return itemList;
-}
-public void setItemList(List<ItemVo> itemList) {
-	this.itemList = itemList;
-}
-public double getTotalPrice() {
-	return totalPrice;
-}
-public void setTotalPrice(double totalPrice) {
-	this.totalPrice = totalPrice;
-}
-public Payment getPayment() {
-	return payment;
-}
-public void setPayment(Payment payment) {
-	this.payment = payment;
-}
+	public List<ItemVo> getItemList() {
+		return itemList;
+	}
+
+	public void setItemList(List<ItemVo> itemList) {
+		this.itemList = itemList;
+	}
+
+	public double getTotalPrice() {
+		return totalPrice;
+	}
+
+	public void setTotalPrice(double totalPrice) {
+		this.totalPrice = totalPrice;
+	}
+
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
+	}
 
 }

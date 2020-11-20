c9bda5641dcc94ed31fb2afea0b3cd8d476feb16
sagardas1 <sagardas.195@gmail.com>
2020-11-20 20:43:10 +0530

    changes

--- ShoppingCartProject/src/main/java/com/shopingCart/userController/UserController.java
+++ ShoppingCartProject/src/main/java/com/shopingCart/userController/UserController.java
@@ -44,6 +44,8 @@ public class UserController {
 	
 	
 	
+	
+	
 	@Transactional
 	@PostMapping(value = "/userregistration", headers = "Accept=application/json")
 	public BaseResponce<Void> userRegistration(@RequestBody RegistrationVo registration) {

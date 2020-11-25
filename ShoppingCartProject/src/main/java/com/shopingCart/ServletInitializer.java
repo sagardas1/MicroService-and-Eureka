f3cd572ea275f3b1ed2e5aad8aef707b3f992128
sagardas1 <sagardas.195@gmail.com>
2020-11-25 21:03:16 +0530

    plainText.text

--- ShoppingCartProject/src/main/java/com/shopingCart/ServletInitializer.java
+++ ShoppingCartProject/src/main/java/com/shopingCart/ServletInitializer.java
@@ -6,6 +6,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 public class ServletInitializer extends SpringBootServletInitializer {
 
 	
+	
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(ShoppingCartProjectApplication.class);
